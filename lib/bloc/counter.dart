6fee7cc8d92108b576970a8b22163f272fbfc9d0
aldoalfians <aldoboetd908@gmail.com>
2023-04-11 00:31:49 +0700

    counter bloc using outside  value

--- lib/bloc/counter.dart
+++ lib/bloc/counter.dart
@@ -4,13 +4,24 @@ import './counter_event.dart';
 class CounterBloc extends Bloc<CounterEvent, int> {
   int init;
   CounterBloc({this.init = 0}) : super(init) {
-    on<CounterEvent>((event, emit) {
-      if (event == CounterEvent.decrement) {
-        emit(state - 1);
-      } else {
-        emit(state + 1);
-      }
+    // on using class
+    on<DecrementCounter>((event, emit) {
+      // ini proses event decrement
+      emit(state - event.value);
     });
+    on<IncrementCounter>((event, emit) {
+      // ini proses event increment
+      emit(state + event.value);
+    });
+
+    // on using enum
+    // on<CounterEvent>((event, emit) {
+    //   if (event == CounterEvent.decrement) {
+    //     emit(state - 1);
+    //   } else {
+    //     emit(state + 1);
+    //   }
+    // });
   }
 
   @override
@@ -26,7 +37,7 @@ class CounterBloc extends Bloc<CounterEvent, int> {
     super.onError(error, stackTrace);
   }
 
-  // Penggunaan transition ini untuk memantau perubahan pada state & event
+  // Penggunaan transition ini untuk memantau perubahan pada event
   // onTrasition hanya ada di penggunaan bloc
   @override
   void onTransition(Transition<CounterEvent, int> transition) {
@@ -43,7 +54,6 @@ class CounterBloc extends Bloc<CounterEvent, int> {
 //     print(error);
 //   }
 // }
-
 
 // Using Cubit
 // class CounterBloc extends Cubit<int> {
