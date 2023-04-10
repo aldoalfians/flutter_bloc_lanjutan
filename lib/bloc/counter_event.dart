34e5ea882fc634314771f4052e770c65b5349f8d
aldoalfians <aldoboetd908@gmail.com>
2023-04-11 00:31:26 +0700

    event bloc using class & create outside value

--- lib/bloc/counter_event.dart
+++ lib/bloc/counter_event.dart
@@ -1,1 +1,17 @@
-enum CounterEvent { decrement, increment }
+// jika yang di butuhkan event tidak kompleks gunakan enum
+// enum CounterEvent { decrement, increment }
+
+// jika event yang dibutuhkan komplek perlu adanya suatu value dari luar gunakan class
+// dengan menggunakan event class dapat menerima sebuah value dari luar
+class CounterEvent {
+  int value;
+  CounterEvent(this.value);
+}
+
+class IncrementCounter extends CounterEvent {
+  IncrementCounter(int value) : super(value);
+}
+
+class DecrementCounter extends CounterEvent {
+  DecrementCounter(int value) : super(value);
+}
